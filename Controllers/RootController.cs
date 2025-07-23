5e132e7b8e2f9e04e2b09c9fabcc1c14e8d11877
Miguel Hernandez <miguel.hernandezg@cecar.edu.co>
2025-07-23 16:21:51 -0500

    feat: agregar RootController y actualizar AccessController

---
+++ Controllers/RootController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiUniRoom;
+using ApiUniRoom.Models;
+
+namespace ApiUniRoom.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API Pension en Render está funcionando 🚀");
+        }
+    }
+
+}
